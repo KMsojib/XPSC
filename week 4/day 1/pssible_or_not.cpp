4577dd6d3656c92b772b45939fd1407b28b72c1c
MD Kawsar Mahmud <kawsarmahmud822@gmail.com>
2023-06-28 11:55:49 +0600

    new

--- week 4/day 1/pssible_or_not.cpp
+++ week 4/day 1/pssible_or_not.cpp
@@ -1,32 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-/*
-5 5
-1 2 3 5 13
-*/
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    int n,k;    cin>>n>>k;
-    vector<int>a(n);
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-    }
-    int ans=-1;
-    for(int i=0;i<n;i++){
-        if((a[i]&k)==k){
-            ans&=a[i];
-        }
-    }
-    if(ans==k){
-        cout<<"YES"<<endl;
-    }else{
-        cout<<"NO"<<endl;
-    }
-    return 0;
+int main() {
+	int test;   cin>>test;
+	while(test--)
+	{
+	    int n,k;    cin>>n>>k;
+	vector<int>a(n) ;
+	for(int i=0;i<n;i++){
+	    cin>>a[i];
+	}
+	int ans=-1;
+	for(int i=0;i<n;i++){
+	    if((a[i]&k)==k){
+	        ans&=a[i];
+	    }
+	}
+	if(ans==k){
+	    cout<<"YES"<<"\n";
+	}else{
+	    cout<<"NO"<<"\n";
+	}
+	}
+	return 0;
 }
