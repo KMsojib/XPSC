6172c1428b5f4dca303520dbbac919f59f7c5754
MD Kawsar Mahmud <kawsarmahmud822@gmail.com>
2023-07-10 23:10:45 +0600

    day 4

---
+++ week 5/day 4/A_candies_and_two_sisters.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        int div=round((n-1)/2);
+        cout<<div<<endl;
+    }
+    return 0;
+}
