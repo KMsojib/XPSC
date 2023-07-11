3e4a1d769b7a0f0039eb1ced23d1eb807743c7b3
MD Kawsar Mahmud <kawsarmahmud822@gmail.com>
2023-07-11 23:15:58 +0600

    day 5

---
+++ week 5/day 5/A_Odd_divisor.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n,i;
+        cin>>n;
+        int flag=0;
+
+        if(n&(n-1)){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
