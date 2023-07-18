f29b8fbca49149341c5645587a9b0ca65bb1a94d
MD Kawsar Mahmud <kawsarmahmud822@gmail.com>
2023-07-18 21:59:52 +0600

    day 6

---
+++ week 6/day 6/Chef_and_Battery.cpp
@@ -0,0 +1,55 @@
+/*
+||-----------------------------------||
+||        MD.Kawsar Mahmud           ||
+||-----------------------------------||
+*/
+
+// #include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+using namespace std;
+typedef long long int ll; 
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;  cin>>t;
+    while(t--){
+        int n;  cin>>n;
+        int ans=0;
+        while(n!=50){
+            if(n>50){
+                n-=3;
+                ans++;
+            }
+            if(n<50){
+                n+=2;
+                ans++;
+            }
+        }
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
