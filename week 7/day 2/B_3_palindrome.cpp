5d2b823c276e19dc6b80f1913b407029a69554d5
MD Kawsar Mahmud <kawsarmahmud822@gmail.com>
2023-07-22 21:10:59 +0600

    day 2

---
+++ week 7/day 2/B_3_palindrome.cpp
@@ -0,0 +1,50 @@
+/*
+||-----------------------------------||
+||        MD.Kawsar Mahmud           ||
+||-----------------------------------||
+*/
+
+// #include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+using namespace std;
+typedef long long int ll; 
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+        int n;  cin>>n;
+        string s[]={"b","b","a","a"};
+        int j=0;
+        for(int i=0;i<n;i++){
+            cout<<s[j];
+            j++;
+            if(j==4){
+                j=0;
+            }
+        }
+        cout<<endl;
+
+    return 0;
+}
