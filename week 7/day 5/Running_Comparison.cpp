b8ca731f78307ca46c5b5e625366c1b05d76b1f9
MD Kawsar Mahmud <kawsarmahmud822@gmail.com>
2023-07-25 23:17:24 +0600

    day 5

---
+++ week 7/day 5/Running_Comparison.cpp
@@ -0,0 +1,57 @@
+/*
+||-----------------------------------||
+||        MD.Kawsar Mahmud           ||
+||-----------------------------------||
+*/
+
+// #include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+using namespace std;
+typedef long long int ll; 
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;  cin>>t;
+    while(t--){
+        int n;  cin>>n;
+        vector<int>a(n),b(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            cin>>b[i];
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){ 
+        if(a[i]<=2*b[i] and b[i]<=2*a[i]){ 
+            ans++; 
+        } 
+        } 
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
---
+++ week 7/day 5/Single_use_Attack.cpp
@@ -0,0 +1,49 @@
+/*
+||-----------------------------------||
+||        MD.Kawsar Mahmud           ||
+||-----------------------------------||
+*/
+
+// #include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+using namespace std;
+typedef long long int ll; 
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;  cin>>t;
+    while(t--){
+        int x,y,z;  cin>>x>>y>>z;
+        int dif=x-z;
+        if(dif%y==0){
+            cout<<1+(dif/y)<<endl;
+        }else{
+            cout<<2+(dif/y)<<endl;
+        }
+    }
+
+    return 0;
+}
