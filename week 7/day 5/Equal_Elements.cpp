7b3cd042cd9932799c08115a2f2707c9fc59c5fa
MD Kawsar Mahmud <kawsarmahmud822@gmail.com>
2023-07-25 23:17:38 +0600

    day 5

---
+++ week 7/day 5/Equal_Elements.cpp
@@ -0,0 +1,53 @@
+/*
+||-----------------------------------||
+||        MD.Kawsar Mahmud           ||
+||-----------------------------------||
+*/
+
+// #include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+using namespace std;
+typedef long long int ll; 
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;  cin>>t;
+    while(t--){
+        int n;  cin>>n;
+        int a[n];
+        vector<int>prf(n+1,0);
+        int cn=0;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            prf[a[i]]++;
+        }
+        sort(prf.begin(),prf.end());
+        int max=prf[n];
+        cout<<n-max<<endl;
+    }
+
+    return 0;
+}
