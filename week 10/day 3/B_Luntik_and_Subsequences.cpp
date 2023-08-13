74fbde2b17c54b39e420beb68ca5b6c57ac7ac60
MD Kawsar Mahmud <kawsarmahmud822@gmail.com>
2023-08-13 22:50:52 +0600

    day 3

---
+++ week 10/day 3/B_Luntik_and_Subsequences.cpp
@@ -0,0 +1,44 @@
+/*
+||-----------------------------------||
+||        MD.Kawsar Mahmud           ||
+||-----------------------------------||
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define in_range(a,b)    for(int i=a;i<b;i++)
+#define read(x) int x; cin >> x
+int main()
+{
+    fast_io;
+    int t;  cin>>t;
+    while(t--){
+        int n;  cin>>n;
+        int cn_0=0,cn_1=0;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            if(a[i]==0) cn_0++;
+            if(a[i]==1) cn_1++;
+        }
+
+        cout<<(1ll<<cn_0)*(ll)cn_1<<endl;
+    }
+
+    return 0;
+}
+/* check constraints
+    * at first try a brute force way to solve
+    * then optimization
+ 
+    *  if its array:
+        1. try greedy
+        2. if it is a result of subproblems try dp
+        3. if it is showing for  a range minimum or maximum
+        3. 1 tttttffffff
+     else
+       3.2 ffffftttttt
+        try binary search
+*/
