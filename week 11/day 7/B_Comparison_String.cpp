b1bc1b6159113552fb29a8c094b702f8ef970cba
MD Kawsar Mahmud <kawsarmahmud822@gmail.com>
2023-08-24 23:22:33 +0600

    day 7

---
+++ week 11/day 7/B_Comparison_String.cpp
@@ -0,0 +1,47 @@
+/*
+||-----------------------------------||
+||        MD.Kawsar Mahmud           ||
+||-----------------------------------||
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define in_range(a,b)    for(int i=a;i<b;i++)
+#define read(x) int x; cin >> x
+int main()
+{
+    fast_io;
+    int t;  cin>>t;
+    while(t--){
+        int n;  cin>>n;
+        string s;   cin>>s;
+        int ans=1,crn=1;
+        for(int i=1;i<n;i++){
+            if(s[i]!=s[i-1]){
+                crn=1;
+            }
+            else{
+                crn++;
+            }
+            ans=max(ans,crn);
+        }
+        cout<<ans+1<<endl;
+    }
+
+    return 0;
+}
+/* check constraints
+    * at first try a brute force way to solve
+    * then optimization
+ 
+    *  if its array:
+        1. try greedy
+        2. if it is a result of subproblems try dp
+        3. if it is showing for  a range minimum or maximum
+        3. 1 tttttffffff
+     else
+       3.2 ffffftttttt
+        try binary search
+*/
