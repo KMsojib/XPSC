15ff0a5086945b4cd164f09bb2d750381d47a41e
MD Kawsar Mahmud <kawsarmahmud822@gmail.com>
2023-09-22 21:12:58 +0600

    day 1

---
+++ week 16/day 1/E_Building_an_Aquarium.cpp
@@ -0,0 +1,61 @@
+/*
+||-----------------------------------||
+||        MD.Kawsar Mahmud           ||
+||-----------------------------------||
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define in_range(a,b)    for(int i=a;i<b;i++)
+#define read(x) int x; cin >> x
+int main()
+{
+    fast_io;
+    int t;  cin>>t;
+    while(t--){
+        long long n,x;
+        //scanf("%lld,%lld",&n,&x);
+        cin>>n>>x;
+
+        vector<long long>a(n);
+        for(int i=0; i<n; i++){
+            //scanf("%lld",&a[i]);
+            cin >>a[i];
+        }
+
+        long long l=1,r=1e10;
+        while(l<r-1){
+            long long total_wt=0;
+            long long mid=l+(r-l)/2;///to avoid overflow
+            for(int i=0; i<n; i++){
+                if(a[i]<mid){
+                    total_wt+=(mid-a[i]);
+                }
+            }
+
+            if(total_wt>x){
+                r=mid;
+            }
+            else{
+                l=mid;
+            }
+        }
+        cout<<l<<"\n";
+    }
+
+    return 0;
+}
+/* check constraints
+    * at first try a brute force way to solve
+    * then optimization
+
+    *  if its array:
+        1. try greedy
+        2. if it is a result of subproblems try dp
+        3. if it is showing for  a range minimum or maximum
+        3. 1 tttttffffff
+     else
+       3.2 ffffftttttt
+        try binary search
+*/
